3c7130736fae3265c628e6db17a6b6a8c3fb21bf
Narges Rezaie <nargesr@uci.edu>
2023-06-01 11:04:13 -0700

    Update __init__.py

--- Topyfic/__init__.py
+++ Topyfic/__init__.py
@@ -1,7 +1,9 @@
 from Topyfic import utils
+from Topyfic.utils.analyseModel import *
 from Topyfic.train import *
 from Topyfic.topic import *
 from Topyfic.topModel import *
 from Topyfic.analysis import *
+from Topyfic.utils.makeModel import *
 
 
